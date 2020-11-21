fb1399b4976a17e2a6cd3d8a5010602916740cec
sean <smapps001@gmail.com>
2020-11-21 15:46:32 -0600

    added notes to installer

--- iComp_install.py
+++ iComp_install.py
@@ -182,6 +182,11 @@ def main():
     makeiCompDirs()
     copyFiles()
     getDbInfo()
+    sys.stdout.write('\n\n')
+    sys.stdout.write('iCompetition web application is now installed!\n')
+    sys.stdout.write('A few notes: - \n')
+    sys.stdout.write('\t - If this is a brand new installation of iComp, please create the iComp database using the script found in /usr/local/lib/iCompetition/mysqlCreateDB.sql')
+    sys.stdout.write('\t - The iCompApi can be started and stopped with iCompApi-up and iCompApi-down commands (You may need to log out and in first) or by using systemctl')
 
 if __name__ == '__main__':
     main()
