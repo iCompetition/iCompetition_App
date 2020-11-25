9a933bdb99cf24277aa7201bb8f5e87969ded32d
sean <smapps001@gmail.com>
2020-11-25 10:30:13 -0600

    adding profile commands

--- iCompSystemFiles/etc/profile.d/iComp.sh
+++ iCompSystemFiles/etc/profile.d/iComp.sh
@@ -1,3 +1,6 @@
 alias iCompApi-up='systemctl start iCompApi'
 alias iCompApi-down='systemctl stop iCompApi'
 alias iCompApi-stat='systemctl status iCompApi'
+alias iCompApi-showLogs='ls -la /var/log/iComp/'
+alias iCompApi-followApiLog='tail -f /var/log/iComp/api.log'
+alias iCompApi-followGunicornLog='tail -f /var/log/iComp/gunicorn.log'
