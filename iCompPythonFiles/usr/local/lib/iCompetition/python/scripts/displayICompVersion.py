3af444f35bf7da2cff0b5d9c2bd502d856f451c3
sean <smapps001@gmail.com>
2021-02-27 21:48:18 -0600

    changing iComp-version command to display both app and db

--- iCompPythonFiles/usr/local/lib/iCompetition/python/scripts/displayICompVersion.py
+++ iCompPythonFiles/usr/local/lib/iCompetition/python/scripts/displayICompVersion.py
@@ -1,5 +1,16 @@
+#!/usr/bin/python3
+
 import sys 
 sys.path.append("/usr/local/lib/iCompetition/python")
+sys.path.append("/usr/local/lib/iCompetition/python/crypt")
 from iComp_util import getConf
-
-sys.stdout.write("\nRunning iCompetition Application Version: " + getConf()['version'] + "\n\n")
+from iComp_db import db_schemaVersion
+from iComp_util import *
+from credManagement import *
+outFmt = ("{:25s}: {}\n")
+roPwd = idecrypt(getCred("iCompRead"))
+schema = db_schemaVersion(roPwd)
+sys.stdout.write("\n")
+sys.stdout.write(outFmt.format("iCompetition App Version", getConf()['version']))
+sys.stdout.write(outFmt.format("iCompeition DB Version", str(schema)))
+sys.stdout.write("\n")
